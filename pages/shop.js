d30829990ee9e72abccd3f84954e071057293ecf
Daan Roelofs <daanroelofs599@gmail.com>
2022-12-02 00:43:23 +0100

    contact form

--- pages/shop.js
+++ pages/shop.js
@@ -1,23 +1,7 @@
+import Form from '../components/Form';
 import styles from '../styles/Shop.module.scss';
 
 const Shop = () => {
-
-
-    async function handleOnSubmit(e) {
-        e.preventDefault();
-        const formData = {}
-        Array.from(e.currentTarget.elements).forEach(field => {
-            if ( !field.name ) return;
-            formData[field.name] = field.value;
-        });
-        fetch('/api/mail', {
-            method: 'post',
-            body: JSON.stringify(formData)
-        })
-        console.log(formData);
-    }
-
-
 
     return (
         <section className={styles.shopcontainer}>
@@ -50,47 +34,9 @@ const Shop = () => {
                         printing and typesetting industry.
                     </p>
                 </div>
-
-
-
                 <div className={styles.formcontainer}>
-                    <style jsx>{`
-                        label {
-                            display: block;
-                            margin-bottom: .2em;
-                            }
-
-                        button {
-                            color: white;
-                            background-color: blueviolet;
-                            padding: .8em 1em;
-                            border: 0;
-                            border-radius: .2em;
-                        }
-                    `}
-                    </style>
-                    
-                    <form method='post' onSubmit={handleOnSubmit}>
-                        <p>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name='name' />
-                        </p>
-                        <p>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name='email' />
-                        </p>
-                        <p>
-                            <label htmlFor="message">Message</label>
-                            <textarea name="message" />
-                        </p>
-                        <p>
-                            <button>Submit</button>
-                        </p>
-                    </form>
+                    <Form />
                 </div>
-
-
-
             </div>
             <div className={styles.chaticon}>
                 <picture>
@@ -100,7 +46,6 @@ const Shop = () => {
                     />
                 </picture>
             </div>
-
         </section>
     );
 }
