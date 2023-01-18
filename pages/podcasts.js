0e76b13e399bb5d5e6c8602a5a30175e267d8569
Daan Roelofs <daanroelofs599@gmail.com>
2023-01-18 19:12:56 +0100

    ajb

--- pages/podcasts.js
+++ pages/podcasts.js
@@ -2,12 +2,10 @@ import { createClient } from 'contentful';
 import PodcastCard from '../components/PodcastCard';
 
 export async function getStaticProps() {
-
     const client = createClient({
         space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
         accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
     })
-
     const res = await client.getEntries({ content_type: 'podcast' })
 
     return {
